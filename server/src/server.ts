1f0350d957ada498e5697ed40d6940d436723225
rodry <rodrymza@gmail.com>
2025-11-16 01:31:14 -0300

    agregadas rutas de usuario

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db';
-
+import usuarioRoutes from './routes/usuarioRoutes';
 dotenv.config();
 
 connectDB();
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 4000;
 app.get('/', (req, res) => {
   res.send('API del Libro de Novedades corriendo...');
 });
+
+app.use("/api/users", usuarioRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
