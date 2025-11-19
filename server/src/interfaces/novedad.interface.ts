e5cd23cf7c22762cfeafbbd80ae902800d41d98b
rodry <rodrymza@gmail.com>
2025-11-18 23:50:40 -0300

    interfaz de novedad

---
+++ server/src/interfaces/novedad.interface.ts
@@ -0,0 +1,33 @@
+import { Types } from "mongoose";
+import { ErrorResponse } from "./error.interface";
+
+export interface CreateNovedad {
+  contenido: string;
+  usuario_id: Types.ObjectId;
+  area_id: Types.ObjectId;
+  etiquetas: string[];
+}
+
+export interface NovedadResponse {
+  id: string;
+  contenido: string;
+  usuario: {
+    id: string;
+    apellido: string;
+    nombre: string;
+    username: string;
+  };
+  area: {
+    id: string;
+    nombre: string;
+  };
+  etiquetas?: string[];
+}
+
+export interface FiltroNovedad {
+  usuario_id: string;
+  area_id: string;
+  tags: string[];
+}
+
+export type NovedadResponseData = NovedadResponse | ErrorResponse;
