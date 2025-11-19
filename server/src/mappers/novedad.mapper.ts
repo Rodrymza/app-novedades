2b8568b85d7723c34d3c694b8a556324bd5a49ac
rodry <rodrymza@gmail.com>
2025-11-18 23:51:07 -0300

    mapper de novedad

---
+++ server/src/mappers/novedad.mapper.ts
@@ -0,0 +1,21 @@
+import { NovedadResponse } from "../interfaces/novedad.interface";
+
+export class NovedadMapper {
+  static toDto(doc: any): NovedadResponse {
+    return {
+      id: doc._id.toString(),
+      contenido: doc.contenido,
+      usuario: {
+        id: doc.usuario?._id.toString() || "Sin ID",
+        nombre: doc.usuario?.nombre || "Usuario",
+        apellido: doc.usuario?.apellido || "Eliminado",
+        username: doc.usuario?.username || "Desconocido",
+      },
+      area: {
+        id: doc.area?._id.toString() || "Sin ID",
+        nombre: doc?.area.nombre || "Area Eliminada",
+      },
+      etiquetas: doc.etiquetas,
+    };
+  }
+}
