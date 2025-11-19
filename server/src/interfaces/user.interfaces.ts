9daf566cbb229c092daea217f5c4a24a595b3c3a
rodry <rodrymza@gmail.com>
2025-11-18 23:50:52 -0300

    interfaz de usuario y auth

--- server/src/interfaces/user.interfaces.ts
+++ server/src/interfaces/user.interfaces.ts
@@ -1,22 +1,27 @@
-import { Types } from 'mongoose';
-import { ErrorResponse } from './error.interfaces';
+import { Types } from "mongoose";
+import { ErrorResponse } from "./error.interface";
 
 export interface CreateUserBody {
-    apellido: string;
-    nombre: string;
-    username: string;
-    email: string;
-    password: string
-    rol?: 'OPERADOR' | 'SUPERVISOR';
+  apellido: string;
+  nombre: string;
+  username: string;
+  email: string;
+  password: string;
+  rol?: "OPERADOR" | "SUPERVISOR";
 }
 
 export interface UserResponse {
-    _id : Types.ObjectId;
-    apellido: string;
-    nombre : string;
-    username: string;
-    email: string;
-    rol: 'OPERADOR' | 'SUPERVISOR';
+  id: Types.ObjectId;
+  apellido: string;
+  nombre: string;
+  username: string;
+  email: string;
+  rol: "OPERADOR" | "SUPERVISOR";
+}
+
+export interface LoginUser {
+  username: string;
+  password: string;
 }
 
 export type UserResponseData = UserResponse | ErrorResponse;
