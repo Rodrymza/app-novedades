09dcaac6b9bc735be750040c77242af2030f7b20
rodry <rodrymza@gmail.com>
2025-11-21 16:26:42 -0300

    agregada ruta de profile

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { crearUsuario, loginUsuario } from "../controller/authController";
+import {
+  crearUsuario,
+  getProfile,
+  loginUsuario,
+} from "../controller/authController";
 import { esSupervisor, validarToken } from "../utils/tokenService";
 
 const authRoutes = Router();
 
-authRoutes.post("/register", validarToken, esSupervisor, crearUsuario)
+authRoutes.post("/register", validarToken, esSupervisor, crearUsuario);
 authRoutes.post("/login", loginUsuario);
+authRoutes.get("/profile", validarToken, getProfile);
 
 export default authRoutes;
