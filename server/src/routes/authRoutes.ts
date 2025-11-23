6f5cd7dd116a6104469db9e2aa90aed4ae666990
rodry <rodrymza@gmail.com>
2025-11-23 18:57:20 -0300

    agregada ruta logout

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import {
   crearUsuario,
   getProfile,
   loginUsuario,
+  logoutUsuario,
 } from "../controller/authController";
 import { esSupervisor, validarToken } from "../utils/tokenService";
 
@@ -11,5 +12,6 @@ const authRoutes = Router();
 authRoutes.post("/register", validarToken, esSupervisor, crearUsuario);
 authRoutes.post("/login", loginUsuario);
 authRoutes.get("/profile", validarToken, getProfile);
+authRoutes.post("/logout", validarToken, logoutUsuario);
 
 export default authRoutes;
