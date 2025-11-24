4cbdf3766a0c97f489122ae76570be7ebcb328d9
rodry <rodrymza@gmail.com>
2025-11-24 14:44:18 -0300

    agregado variable para perfil de usuario

--- client/src/hooks/useUsers.ts
+++ client/src/hooks/useUsers.ts
@@ -6,6 +6,7 @@ export const useUsers = () => {
   const [usuarios, setUsuarios] = useState<UserResponse[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [perfil, setPerfil] = useState<UserResponse | null>(null);
 
   const traerUsuarios = useCallback(async () => {
     try {
@@ -22,10 +23,28 @@ export const useUsers = () => {
       setLoading(false);
     }
   }, []);
+
+  const getPerfil = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const perfil = await UserService.getProfile();
+      if (perfil) {
+        setPerfil(perfil);
+      }
+    } catch (error) {
+      setError("No se pudo cargar el perfil de usuario");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return {
     error,
     traerUsuarios,
     usuarios,
     loading,
+    getPerfil,
+    perfil,
   };
 };
