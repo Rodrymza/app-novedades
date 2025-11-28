049532a7c6617aa8281d9e0569ffbaf295dac5b2
rodry <rodrymza@gmail.com>
2025-11-28 20:39:08 -0300

    agregada rua de eliminacion

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { findAllUsers, getUsersList } from "../controller/userController";
+import {
+  eliminarUsuario,
+  findAllUsers,
+  getUsersList,
+} from "../controller/userController";
 import { esSupervisor, validarToken } from "../utils/tokenService";
 
 const userRoutes = Router();
 
 userRoutes.get("/", validarToken, esSupervisor, findAllUsers);
 userRoutes.get("/user-list", validarToken, getUsersList);
+userRoutes.delete("/", validarToken, esSupervisor, eliminarUsuario);
 
 export default userRoutes;
