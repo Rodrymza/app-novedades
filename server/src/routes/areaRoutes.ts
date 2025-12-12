c049112183a2aba6fe217f0d24657538acc5282f
rodry <rodrymza@gmail.com>
2025-12-12 01:11:50 -0300

    creacion de ruta para restaurar area, mejora ruta de eliminacion

--- server/src/routes/areaRoutes.ts
+++ server/src/routes/areaRoutes.ts
@@ -3,13 +3,28 @@ import {
   crearArea,
   eliminarArea,
   findallAreas,
+  restaurarArea,
 } from "../controller/areaController";
 import { esSupervisor, validarToken } from "../utils/tokenService";
+import { validarIdMongo } from "../middlewares/validaciones.middleware";
 
 const areaRoutes = Router();
 
 areaRoutes.get("/", validarToken, findallAreas);
 areaRoutes.post("/", validarToken, esSupervisor, crearArea);
-areaRoutes.put("/eliminar", validarToken, esSupervisor, eliminarArea);
+areaRoutes.patch(
+  "/:id/eliminar",
+  validarToken,
+  esSupervisor,
+  validarIdMongo,
+  eliminarArea
+);
+areaRoutes.patch(
+  "/:id/restaurar",
+  validarToken,
+  esSupervisor,
+  validarIdMongo,
+  restaurarArea
+);
 
 export default areaRoutes;
