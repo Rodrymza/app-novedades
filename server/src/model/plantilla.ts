2199c70a958ad019a6581b7624ccd2a47df6e108
rodry <rodrymza@gmail.com>
2026-02-10 00:41:19 -0300

    modelo de plantilla para mongoose

---
+++ server/src/model/plantilla.ts
@@ -0,0 +1,38 @@
+import { HydratedDocument, Schema, model } from "mongoose";
+import { PrioridadNovedad } from "../interfaces/novedad.interface";
+
+export interface IPlantilla {
+  nombre: string;
+  tags: string[];
+  contenido: string;
+  prioridad: PrioridadNovedad;
+  is_deleted: boolean;
+}
+
+const plantillaSchema = new Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  contenido: {
+    type: String,
+    required: true,
+  },
+  prioridad: {
+    type: String,
+    enum: Object.values(PrioridadNovedad),
+    default: PrioridadNovedad.NORMAL,
+  },
+  is_deleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default model("Plantillla", plantillaSchema);
+export type IPlantillaDocument = HydratedDocument<IPlantilla>;
